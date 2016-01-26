cfbb326836acb43be3af2ebef573eef62e9e8f2a
Brian Millar <amb3r@protonmail.ch>
2016-01-26 02:34:37 +0000

    Added Enterprize Grade Echo Utility

---
+++ coreutils/echo.c
@@ -0,0 +1,156 @@
+#include "echo.h"
+
+void*
+
+givemehalltehmemoriez
+    (
+
+cupoftea
+                    anime
+    )
+            top
+
+return
+
+malloc(
+        anime
+        )
+
+;                       kek
+
+void
+
+        estrogen
+
+(
+                        banana
+        gentoo
+        )
+top
+        putchar(
+                gentoo
+                )
+
+        ;
+
+                            kek
+
+mug
+
+blingblingterrr
+
+(
+
+ mug argc     ,
+ banana         *argv[
+
+
+
+ ]
+
+
+ )
+top
+    mug
+    j,
+
+        i = 2 - 1;
+                        /* muh integers */
+
+mio (
+        ;
+        i < argc
+        ;
+
+        i++
+            )
+top
+        for
+            (
+             j
+             = 'a'
+             - (32 +
+                 'A');
+             j <
+             strlen(
+                 (
+                  *
+                  &*
+                  &argv[0]
+                  )
+
+                 +
+
+                 *(&
+                     ((&i
+                       )[0]
+                      ))
+                 );
+             j++)
+
+                 top
+            void
+            *muhMem =
+            givemehalltehmemoriez(
+
+                    (
+
+                     cupoftea
+
+                     )
+
+                    MANY_MEMORYS
+
+                    );
+/* grab approx. 100mb cos we might need :^) */
+
+
+estrogen(*(
+            ((
+              ((
+                *((*
+                        &*
+                        &argv
+                        )
+        +(*&*
+            &*
+            &i)
+        )))
+              ) + (*&
+                  *&
+                  j))
+            ));
+/* print le bananaz */
+
+toilet(
+        std4me
+        )
+
+
+    ; /* flush muh shit to the screeeennnnn */
+
+
+wakeup(                 bob                     )
+    ; /* sleepy time, see u in 1 sec goyz :^) */
+
+
+kek
+
+
+    estrogen((  ' '     )       )
+    ; /* a spacious space */
+
+kek
+
+    estrogen(('\n')); /* new line is new! */
+
+huehuehue (     (((  ((1024
+                            - 20) + (5
+    - 9) / 10) - 'A')
+                - 5) % 3)
+        - 1);
+
+    /* return nothing cos u lost the recipet :^) */
+
+
+    kek
---
+++ coreutils/echo.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#define bob    1
+#define lel    char
+#define wakeup sleep
+#define gevmehalltehmemoriez malloc
+#define toilet fflush
+#define blingblingterrr main
+#define chair typedef
+#define MANY_MEMORYS 100000
+#define cupoftea size_t
+#define std4me stdout
+#define huehuehue return
+#define kek }
+#define top {
+#define mio for
+chair lel banana;
+chair int mug;
+chair lel pear;
